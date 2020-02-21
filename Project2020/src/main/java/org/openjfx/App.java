cd774e53e67fa7d508a6c31ff898bf778dd6e873
Jørgen Rosager <joergen.rosager@gmail.com>
2020-02-21 13:52:39 +0100

    Add Ricardo

--- Project2020/src/main/java/org/openjfx/App.java
+++ Project2020/src/main/java/org/openjfx/App.java
@@ -4,8 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -13,23 +18,41 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    //private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
+
+        StackPane root = new StackPane();
+        File file = new File("src/main/java/org/openjfx/ricardo.mp4");
+        Media media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+        // MediaPlayer player = new MediaPlayer( new Media(getClass().getResource("/Users/jorgenr/TDT4145/Project2020/src/main/java/org/openjfx/ricardo.mp4").toExternalForm()));
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        root.getChildren().add( mediaView);
+
+        Scene scene = new Scene(root, 1024, 768);
+
         stage.setScene(scene);
         stage.show();
+
+
+        mediaPlayer.play();
+        // scene = new Scene(loadFXML("primary"));
+        // stage.setScene(scene);
+       // stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    //static void setRoot(String fxml) throws IOException {
+    //    scene.setRoot(loadFXML(fxml));
+    //}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    //private static Parent loadFXML(String fxml) throws IOException {
+    //    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    //    return fxmlLoader.load();
+    //}
 
     public static void main(String[] args) {
         DBConnector conn = new DBConnector();
--- Project2020/src/main/java/org/openjfx/PrimaryController.java
+++ Project2020/src/main/java/org/openjfx/PrimaryController.java
@@ -2,11 +2,16 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    //@FXML
+    //private void switchToSecondary() throws IOException {
+        //App.setRoot("secondary");
+    //}
 }
