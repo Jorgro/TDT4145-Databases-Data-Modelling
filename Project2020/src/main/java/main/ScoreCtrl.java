c7acf15deadcf24cafb5950ecfa9530c9c65345c
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-03-12 11:53:50 +0100

    Feat: add initial score features

--- Project2020/src/main/java/main/ScoreCtrl.java
+++ Project2020/src/main/java/main/ScoreCtrl.java
@@ -23,7 +23,33 @@ public class ScoreCtrl extends DBConnector {
         return -1;
     }
 
+    /**
+     * Part of usecase 4, link score to title
+     * @param scoreID
+     * @param titleID
+     * @throws SQLException
+     */
     public void linkScoreTitle(int scoreID, int titleID) throws SQLException {
-        
+        PreparedStatement prep = conn.prepareStatement("INSERT INTO scoreInTitle (TitleID, ScoreID) VALUES (?, ?);");
+        prep.setInt(1, titleID);
+        prep.setInt(2, scoreID);
+    }
+
+    /**
+     * Part of usecase 4, link score and person with a given role
+     * @param personID
+     * @param scoreID
+     * @param role
+     * @throws SQLException
+     */
+    public void linkPersonScore(int personID, int scoreID, String role) throws SQLException {
+        PreparedStatement prep = conn.prepareStatement("INSERT INTO roleInScore (PersonID, ScoreID, Role) VALUES (?, ?, ?)");
+        prep.setInt(1, personID);
+        prep.setInt(2, scoreID);
+        prep.setString(3, role);
+    }
+
+    public void interactWithScoreThroughConsole(){
+        //TODO
     }
 }
